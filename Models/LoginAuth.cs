fe46820464331e7c89769030c3cf671a34b31e0d
FabianSalcedo3 <fabian.salcedo3@gmail.com>
2023-09-25 15:18:49 +0200

    Server Commit

---
+++ Models/LoginAuth.cs
@@ -0,0 +1,9 @@
+﻿namespace PassePartoutApi.Models
+{
+    public class LoginAuth
+    {
+        //Non da includere in data_access
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ data_access/UtenteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassePartoutApi.Models;
+
+namespace PassePartoutApi.data_access
+{
+    public class UtenteDbContext:DbContext
+    {
+        public UtenteDbContext(DbContextOptions<UtenteDbContext>options):base(options)
+        { }
+
+        public DbSet<Utente> Utente { get; set; } //includo il modello Utente per la migrazione
+    }
+}
